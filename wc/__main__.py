f166071b07a61b56f7e93e8a48aaa364179bdd37
Epistaxiophobic <xerotheprogrammer@gmail.com>
2024-11-17 23:08:54 +0330

    add max line length functionality

--- wc/__main__.py
+++ wc/__main__.py
@@ -12,12 +12,14 @@ def get_details(path):
             "words": 0,
             "bytes": 0,
             "chars": 0,
+            "max_length": 0,
             }
 
     try:
         details["bytes"] = getsize(path)
         file = open(path)
         for line in file:
+            details["max_length"] = max(details["max_length"], len(line[:-1]))
             in_word = 0
             for c in line:
                 details["chars"] += 1
@@ -58,6 +60,7 @@ def main():
     parser = argparse.ArgumentParser(description="A rewrite of Unix wc utility in python")
     parser.add_argument("-l", "--lines", action="store_true", help="print the newline counts")
     parser.add_argument("-m", "--chars", action="store_true", help="print the character counts")
+    parser.add_argument("-L", "--max-line-length", action="store_true", help="print the maximum display width")
     parser.add_argument("-c", "--bytes", action="store_true", help="print the byte counts")
     parser.add_argument("-w", "--words", action="store_true", help="print the word counts")
     parser.add_argument("FILE", type=str, nargs="*", help="With no FILE, or when FILE is -, read standard input")
@@ -69,7 +72,7 @@ def main():
         if is_valid_file(path):
             file_details = get_details(path)
             if not file_details["err"]:
-                print(file_details["new_lines"], file_details["words"], file_details["bytes"], file_details["chars"], path)
+                print(file_details["new_lines"], file_details["words"], file_details["bytes"], file_details["max_length"], path)
 
 if __name__ == "__main__":
     main()
